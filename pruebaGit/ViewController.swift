b70879e0b2248033949ec5dcd11b6d571da41191
Giovanni Manjarrez Montelongo <gi0_m0nte@hotmail.com>
2016-02-08 21:25:04 -0700

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Giovanni Manjarrez Montelongo on 08/02/16.
+//  Copyright © 2016 Giovanni Manjarrez Montelongo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
